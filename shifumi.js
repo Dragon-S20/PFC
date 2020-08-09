9f5b10275ba609631c80a7a102c4be213bc8705f
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2020-08-09 22:48:58 +0200

    Js corrected

--- shifumi.js
+++ shifumi.js
@@ -1,56 +1,49 @@
-const game = ()=> {
+const game = () => {
 let pScore = 0;
 let cScore = 0;
 
 const startGame = () => {
-    constplayBtn = document.querySelector(".intro button")
+    const playBtn = document.querySelector(".intro button")
     const introScreen = document.querySelector(".intro")
     const match = document.querySelector(".match")
 
     playBtn.addEventlistener("click", () => {
         introScreen.classList.add("fadeOut");
         match.classList.add("fadeIn")
-    })
+    });
  };
-
- const updateScore = () =>{
-     const playerScore = document.querySelector('.player-score p')
-     const computerScore = document.querySelector('.computer-score p')
-    playerScore.textContent = pScore;
-    computerScore.textContent = cScore;
-
-    }
+ 
  // Jeu en marche
 
  const playMatch = () => {
     const options = document.querySelectorAll(".options button");
-    const playHand = document.querySelector(".player-hand")
-    const computerHand = document.querySelector(".computer-hand")
-    const hands = document.querySelectorAll('.hands img')
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
 
     hands.forEach(hand =>{
-        hands.addEventListener('animationed', function(){
+        hands.addEventListener('animationend', function () {
             this.style.animation = '';
         });
     });
 
     // Option du PC
-    const computerOptions = ['rock', 'paper', 'scissors'];
+    const computerOptions = ["rock", "paper", "scissors"];
 
     options.forEach(options => {
-        options.addEventListener("click", function () => {
+        options.addEventListener("click", function () {
             // Choix du PC
             const computerNumber = Math.floor(Math.random() * 3);
             const computerChoice = computerOptions[computerNumber];
 
-            setTimeout(() =>{
+            setTimeout(() => {
         //Faire fonctionner les mains
         compareHands(this.textContent, computerChoice);
     
     // chargement image des mains
     playerHand.src = `/assets/${this.textContent}.png`;
     computerHand.src = `/assets/${computerChoice}.png`;
-            }, 2000)
+            }, 2000);
 
             //animation
     playerHand.style.animation = "shakePlayer 2s ease";
@@ -59,60 +52,68 @@ const startGame = () => {
     });
  };
 
- const comparehands = (playerChoice, computerChoice) =>{
-     const winner = document.querySelector('.winner')
-     // fonction vérifie l'égalité
-     if(playerChoice === computerChoice){
-         winner.textContent = 'Egalité';
-         return;
+    const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p")
+    const computerScore = document.querySelector(".computer-score p")
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+   }
 
+    const comparehands = (playerChoice, computerChoice) => {
+     const winner = document.querySelector(".winner")
+     // fonction vérifie l'égalité
+     if (playerChoice === computerChoice) {
+         winner.textContent = "Egalité";
+         return;
      }
      //fonction vérifie pour pierre
-     if (playerChoice === 'rock'){
-         if(computerChoice === 'scissors'){
-             winner.textContent = 'Joueur gagne';
+     if (playerChoice === "rock") {
+         if (computerChoice === "scissors") {
+             winner.textContent = "Joueur gagne";
              pScore++;
              updateScore();
-         } else{
-             winner.textContent = 'Ordinateur gagne';
+             return;
+         } else {
+             winner.textContent = "Ordinateur gagne";
              cScore++;
-             updateScore();
-             return
+             updateScore ();
+             return;
          }
      }
     // vérifier pour le choix du papier
  }
- if (playerChoice === 'papier'){
-    if(computerChoice === 'scissors'){
+ if (playerChoice === "paper"){
+    if (computerChoice === "scissors"){
         winner.textContent = 'Ordinateur gagne';
         cScore++;
         updateScore();
-    } else{
+        return;
+    } else {
         winner.textContent = 'joueur gagne gagne';
         pScore++;
         updateScore();
-        return
+        return;
     }
 }
 
     // fonction vérifie les ciseaux
 
-if (playerChoice === 'scissors'){
-    if(computerChoice === 'rock'){
-        winner.textContent = 'Ordinateur gagne';
+if (playerChoice === "scissors") {
+    if (computerChoice === "rock") {
+        winner.textContent = "Ordinateur gagne";
         cScore++;
         updateScore();
-    } else{
-        winner.textContent = 'Joueur gagne';
+        return;
+    } else {
+        winner.textContent = "Joueur gagne";
         pScore++;
         updateScore();
-        return
+        return;
     }
 }
     startGame();
+    playMatch();
     updateScore();
-
-
-}
+};
 
 game ()
